1d2068296e4586b6fc690a9826c9e468d5a3a10c
ProfessionalDevices <castielloluigi89@gmail.com>
2023-11-29 13:48:06 +0100

    first release

---
+++ routes/auth.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post("/login",
+  [
+    body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email address.')
+    .normalizeEmail(),
+    body("password", 'password has to be valid.')
+    .isLength( {min: 5})
+    .isAlphanumeric()
+    .trim()
+  ],
+  authController.postLogin);
+
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid Email.")
+      .custom((value, { req }) => {
+        // if (value === "test@test.com") {
+        //   throw new Error("This Email is forbidden.");
+        // }
+        // return true;
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "E-Mail already exists, please pick a different one"
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+    body(
+      "password",
+      "Please enter a password with only number and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword").trim().custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("The passwords have to match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
+
+module.exports = router;
+
+// validation le< 14
